0401c04117cbc2c2432b12066fe10cf2be5414ad
RuffLeader <hmagerstein01@gmail.com>
2025-12-17 12:02:25 +1100

    Update bcadvent.js

--- blog/beer-cartel-advent-2025/bcadvent.js
+++ blog/beer-cartel-advent-2025/bcadvent.js
@@ -1,9 +1,9 @@
 const adventBeers = [
   {
     day: 1,
-    image: "/media/advent-bc/day1.jpg",
+    image: "/media/advent-bc/day-1.jpg",
     beerName: "Sauvin Ice",
-    brewery: "Mountain Culture Beer Co",
+    brewery: "Mountain Culture Beer Co.",
     style: "Double Cold IPA",
     abv: "8%",
     blurb: "This Double Cold IPA starts with a light malt base, fermented using our house lager yeast for a clean, refined body. A huge double dry hop of Cryo Nelson Sauvin from New Zealand provides gooseberries, passionfruit and lychee that is perfect for our warm festive season. Bright, crisp and dry body showcasing pure hop expression. Nelson Sauvin delivers gooseberry, passionfruit and lychee with hints of tropical fruit. Firm bitterness and clean finish make it sharp, aromatic and highly drinkable.",
@@ -23,9 +23,9 @@ const adventBeers = [
     }
   },
 
-    {
+  {
     day: 2,
-    image: "/media/advent-bc/day2.jpg",
+    image: "/media/advent-bc/day-2.jpg",
     beerName: "Tundra Struck",
     brewery: "KAIJU! Beer",
     style: "Cold Pale Ale",
@@ -47,6 +47,294 @@ const adventBeers = [
     }
   },
 
+  {
+    day: 3,
+    image: "/media/advent-bc/day-3.jpg",
+    beerName: "Bierchen #1",
+    brewery: "Molly Rose Brewing",
+    style: "Altbier",
+    abv: "4.8%",
+    blurb: "A beautiful amber bier made with lager brewing techniques to imitate a lager but also be an ale in the style of a Dusseldorf Altbier. Pale and roasted Voyager malts give a rich but clean malt character and a traditional German Ale yeast fermented cold and cold conditioned for weeks and weeks give the clean refreshing finish. A perfect pairing to a Roast Turkey/Duck/Goose/Ham or the end of a long day.",
+
+    scores: {
+      advertised: 4,
+      canArt: 3.25,
+      blurb: 3.75,
+      value: 3.25,
+      untappd: 3.75,
+      total: 18
+    },
+
+    reviews: {
+      hudson: "Toned down malt and biscuit flavours, reminiscent of a classic Amber Ale but ultimately dialed back. Right up my alley flavour wise but would love it to be bolder. May just be the Altbier style, of which I’m not completely familiar.",
+      sarah: "*Stares blankly into void* *Still staring* *Shrugs shoulders* Bit plain, it just tastes like a beer."
+    }
+  },
+
+  {
+    day: 4,
+    image: "/media/advent-bc/day-4.jpg",
+    beerName: "The Shimmering Sparkling Ocean Ale",
+    brewery: "Sailors Grave Brewing",
+    style: "Sparkling Ocean Ale",
+    abv: "5%",
+    blurb: "An Australian Sparkling Ale brewed with fresh seawater from the Cape Conran Coastline down in Sou' East Gippsland.  Floral, estery and crisp with a light malt backbone and a lick of salty tang.",
+
+    scores: {
+      advertised: 4.5,
+      canArt: 3.75,
+      blurb: 3.75,
+      value: 3.25,
+      untappd: 3.5,
+      total: 18.75
+    },
+
+    reviews: {
+      hudson: "Summer in a can, this makes me think I’m sitting at the Aus Open on Show Court 1 watching the 30th seed face an unseeded Russian guy. Probably cos I did that one time whilst drinking a Coopers Sparkling Ale.",
+      sarah: "Ough, that’s nice… but like hit the back of my throat, like, the flavour. Is it sour? Slightly? I don’t know if sours the right word. Yeah, not bad."
+    }
+  },
+
+  {
+    day: 5,
+    image: "/media/advent-bc/day-5.jpg",
+    beerName: "Who Would Want To Soak A Rope In Kerosene",
+    brewery: "Banks Brewing",
+    style: "West Coast IPA",
+    abv: "6.2%",
+    blurb: "This West Coast IPA is bright, clear and brimming with hop intensity, bringing resinous pine, zesty citrus and ripe tropical fruit to the fore.",
+
+    scores: {
+      advertised: 5,
+      canArt: 3.25,
+      blurb: 4,
+      value: 4,
+      untappd: 4.75,
+      total: 21
+    },
+
+    reviews: {
+      hudson: "Wowee, what a West Coast! An abundance of pine, and fruit, this is as good as it can get. I would honestly be happy with an Advent Calendar that just turns out to be 24 of these.",
+      sarah: "That’s nice, I like it. It’s just a really nice tasting beer."
+    }
+  },
+
+  {
+    day: 6,
+    image: "/media/advent-bc/day-6.jpg",
+    beerName: "Contested Origins",
+    brewery: "Hawkers Beer",
+    style: "Oat Cream IPA",
+    abv: "8%",
+    blurb: "Where did the legendary Pavlova originate? Some say NZ. Some say Australia. You know the correct answer in your heart, and your heart alone. Dry-hopped with Mosaic and complemented by additions of passionfruit and vanilla, the origin of your enjoyment is this can.",
+
+    scores: {
+      advertised: 5,
+      canArt: 3.75,
+      blurb: 4.5,
+      value: 5,
+      untappd: 4,
+      total: 22.25
+    },
+
+    reviews: {
+      hudson: "Very fruity, very sweet, the combination of pavlova and oat cream IPA has it so sweet that it tastes thicker than a standard beer. A huge bundle of flavours like your favourite Christmas Pav should be.",
+      sarah: "*Contemplates* It’s nice, but I don’t know what the flavour is. *Goes for round 2 for the first time ever, huge scenes* I like it, but I don’t know what the fruit is. Like a creamy peach is what I’m tasting."
+    }
+  },
+
+  {
+    day: 7,
+    image: "/media/advent-bc/day-7.jpg",
+    beerName: "Last Exit West",
+    brewery: "Range Brewing",
+    style: "California IPA",
+    abv: "6.6%",
+    blurb: "A crisp & fruity Cali IPA brewed for the Beer Cartel Advent Calendar. We've gone with a heavy dry-hop of Rakau, Eclipse & Krush to deliver waves of tropical and citrus fruits, with a little hit of NZ dank to round things out.",
+
+    scores: {
+      advertised: 4.25,
+      canArt: 3.5,
+      blurb: 4,
+      value: 4,
+      untappd: 4,
+      total: 19.75
+    },
+
+    reviews: {
+      hudson: "This has similar tones to a Cold IPA or West Coast Pilsner, light and refreshing, but still hosting the notable flavours that reflect the West Coast style.",
+      sarah: "That’s nice. It’s kind of all I have to say on that. It’s nice, nice light beer."
+    }
+  },
+
+  {
+    day: 8,
+    image: "/media/advent-bc/day-8.jpg",
+    beerName: "American Brown Ale",
+    brewery: "Philter Brewing",
+    style: "American Brown Ale",
+    abv: "5.5%",
+    blurb: "A classic malt driven American Brown Ale. Chocolate, caramel and nutty, mild bitterness and distinct US Hop flavour.",
+
+    scores: {
+      advertised: 5,
+      canArt: 3.5,
+      blurb: 4.25,
+      value: 3.75,
+      untappd: 4.75,
+      total: 21.25
+    },
+
+    reviews: {
+      hudson: "Toffee, malt and biscuits, oh my! This is one I could drink forever. I saw, on the line, that some people’s cans were a bit how ya goin’, luckily, mine was not. And it was filled with this nectar.",
+      sarah: "I don’t like dark beers, but it tastes a bit nutty. I don’t really like dark beers."
+    }
+  },
+
+  {
+    day: 9,
+    image: "/media/advent-bc/day-9.jpg",
+    beerName: "Little Hopper",
+    brewery: "Ocean Reach Brewing",
+    style: "Black IPA",
+    abv: "7%",
+    blurb: "Bold yet balanced, Little Hopper struts in with roasted coffee & dark chocolate malts, then cheekily flips the script with a lively mix of hops. Zesty citrus, tropical fruit, & pine tease their way through the smooth malt backbone, finishing crisp and complex. Built on a base of pale and roasted malts, this beer delivers a smooth roast character with notes of coffee and dark cocoa. Late additions of Mosaic, Citra, and Simcoe hops contribute bright layers of citrus, tropical fruit, and pine. Fermented clean with American ale yeast to highlight balance between the malt richness and hop expression. Finishes dry with moderate bitterness and a crisp edge.",
+
+    scores: {
+      advertised: 4.5,
+      canArt: 3.75,
+      blurb: 4.25,
+      value: 4.75,
+      untappd: 4.25,
+      total: 21.5
+    },
+
+    reviews: {
+      hudson: "Full of roasted coffee it seems. But the classic subtle hoppy flavours. Unfortunately, with Black IPAs the roasty flavours are usually so strong it’s difficult to get much else. Fortunately, I love that flavour, so this is yum as!",
+      sarah: "“*Recoiled at the smell* *Shuddered after tasting* *Began laughing* What are you writing? I don’t really like dark beers, I’m sorry."
+    }
+  },
+
+  {
+    day: 10,
+    image: "/media/advent-bc/day-10.jpg",
+    beerName: "Deck The Helles",
+    brewery: "White Bay Brewery",
+    style: "Helles Lager",
+    abv: "4.7%",
+    blurb: "A clean drinking Helles Lager that melds old world malt with new world malts from New Zealand. Built on a solid base of Noble German varieties Hallertauer, Mittelfrüh and Spalt. A no nonsense easy drinking lager with a golden hue that you could easily find in Munich's Hofbrauhaus.",
+
+    scores: {
+      advertised: 4.75,
+      canArt: 4,
+      blurb: 4,
+      value: 3.25,
+      untappd: 4.5,
+      total: 20.5
+    },
+
+    reviews: {
+      hudson: "All the notes of a lager, but thicker and juicier. I don’t know if it’s the Helles or just me, but I get banana bread out of this one.",
+      sarah: "That’s nice, that’s really nice. *When asked for tasting notes* I don’t know flavours, it tastes like beer."
+    }
+  },
+
+  {
+    day: 11,
+    image: "/media/advent-bc/day-11.jpg",
+    beerName: "Altbier",
+    brewery: "Reckless Brewing Co.",
+    style: "Red Ale",
+    abv: "4.7%",
+    blurb: "Forget the tinsel and glitter - this beer is all about tradition. Albier (literally “Old Beer”) hails from Düsseldorf, Germany, where the locals have been perfecting it for centuries. Malty, toasty, with a crisp finish that sneaks in like the last carol of the night. Crack it, raise a glass & lets countdown to Christmas! A dark copper-colored ale, known for being a malty and smooth, yet balanced beer with a crisp, lager-like finish. It is fermented with top-fermenting yeast at a moderate temperature but then cold-aged, which results in its brilliant clarity and clean taste.",
+
+    scores: {
+      advertised: 4.25,
+      canArt: 5,
+      blurb: 4.5,
+      value: 3.5,
+      untappd: 3.75,
+      total: 21
+    },
+
+    reviews: {
+      hudson: "Malty as ever, this is very reminiscent of a Red Ale, and I’m probably showing my lack of knowledge saying that. It does taste smokier than a standard red though.",
+      sarah: "“*Owl hoot* Has that got whisky in it? No, not whisky, something strong. Bourbon or something? It’s got a smokey whisky flavour."
+    }
+  },
+
+  {
+    day: 12,
+    image: "/media/advent-bc/day-12.jpg",
+    beerName: "Triple Decoction Czech Pilsner",
+    brewery: "Devilbend Farm Beer Co",
+    style: "Czech Pilsner",
+    abv: "5%",
+    blurb: "Czech Pilsner is brewed with a triple decoction process. Brewed with our farm-grown pilsner malt and using the traditional triple decoction method, this Czech Pilsner is a rich expression of heritage and craft. Deep malt complexity meets a crisp, clean finish, highlighting the grain’s full flavor. Smooth, golden, and refined. This lager is a tribute to classic technique and homegrown ingredients.",
+
+    scores: {
+      advertised: 4.75,
+      canArt: 3.75,
+      blurb: 4.25,
+      value: 3.5,
+      untappd: 4.5,
+      total: 20.75
+    },
+
+    reviews: {
+      hudson: "Genuinely my favourite lager style, the triple decoction also seems to add like a marzipan type flavour. Luscious and crispy.",
+      sarah: "Quite nice, it feels very light. Is it light? Doesn't have a strong beer taste which is nice. My kind of beer."
+    }
+  },
+
+  {
+    day: 13,
+    image: "/media/advent-bc/day-13.jpg",
+    beerName: "Surfin' Santas",
+    brewery: "The Coastal Brewing Company",
+    style: "Double Red IPA",
+    abv: "8.4%",
+    blurb: "A bold and expressive Double Red IPA that strikes the perfect balance between deep malt richness and hop intensity meaning that whatever was on your Christmas wish list you will be happy with what the Surfin’ Santas delivered! Brewed with Citra, Cryo Mosaic and Simcoe hops.",
+
+    scores: {
+      advertised: 5,
+      canArt: 4,
+      blurb: 3,
+      value: 5,
+      untappd: 4.75,
+      total: 21.75
+    },
+
+    reviews: {
+      hudson: "This beer is the colour that Simmo says his hair is, whenever someone calls him a ginger. 'It's Auburn' he woudl tell everyone. Flavour wise, this is an absolute gem, subtly malty, but stone fruit seems to come to the fore. Much like Simmo's auburn hair, this is wonderful. Having said Auburn however many times now, I think Simmo's new nickname should be Aubie, after the Auburn Tigers mascot. Because Simmo seems to do as much work promoting Auburn as their mascot does.",
+      sarah: "*Shakes head* I don't like that."
+    }
+  },
+
+  {
+    day: 14,
+    image: "/media/advent-bc/day-14.jpg",
+    beerName: "Ode To Bitter",
+    brewery: "Slow Lane Brewing",
+    style: "Hoppy Belgian Blonde",
+    abv: "5%",
+    blurb: "An ode to bitterness, Belgian style. This beer is our tribute to the sharp, hoppy edge that’s often hidden beneath the softer side of Belgian ales. Crisp, complex and uncompromising, Ode to Bitter is here to bring bitterness back into fashion. Brewed with a traditional Belgian yeast strain, it delivers lively aromas of spice and fruit, before giving way to a firm, lingering hop bitterness.",
+
+    scores: {
+      advertised: 4.5,
+      canArt: 3.75,
+      blurb: 4.25,
+      value: 3.5,
+      untappd: 3.5,
+      total: 19.5
+    },
+
+    reviews: {
+      hudson: "This is a weird pairing for me, Belgian ale flavours, but hoppy. Not Bad. Good review, Hudson.",
+      sarah: "Eugh, looking at it, it's very see through, so I thought it'd be weak. But it has a very strong flavour, it's not good."
+    }
+  },
+  
   {
     day: 2,
     image: "/media/advent-bc/day2.jpg",
